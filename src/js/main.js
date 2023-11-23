986efbb2d03383368317a98da8c1f7183042d7c9
Dane <wattsd1985@gmail.com>
2023-11-23 12:34:35 -0500

    Completed Updates

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,18 @@
 import * as bootstrap from "bootstrap";
 import * as aos from "aos";
 
-console.log('Hello')
+console.log("Hello");
+
+if (module.hot) {
+  module.hot.dispose(function (data) {
+    // module is about to be replaced.
+    // You can save data that should be accessible to the new asset in `data`
+    data.updated = Date.now();
+  });
+
+  module.hot.accept(function (getParents) {
+    // module or one of its dependencies was just updated.
+    // data stored in `dispose` is available in `module.hot.data`
+    let { updated } = module.hot.data;
+  });
+}
