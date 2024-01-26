ba08044ee85f5de02d5ab43ae74b30ef7858d40f
Dane <wattsd1985@gmail.com>
2024-01-26 02:06:16 -0500

    Spotify Height

--- src/js/main.js
+++ src/js/main.js
@@ -75,7 +75,7 @@ navigation.innerHTML = ` <div id="nav" class="container-fluid">
                   style="border-radius: 12px"
                   src="https://open.spotify.com/embed/playlist/1rHOk7zzyBgF9dqRIN56Sz?utm_source=generator&theme=0"
                   width="100%"
-                  height="352"
+                  height="10%"
                   frameborder="0"
                   allowfullscreen=""
                   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
